dfec3e3543626921e8a85cacd18b0eda2800f7ac
marettialine <alinesmaretti@gmail.com>
2023-07-11 17:09:42 -0300

    Arquivos de estilização
    Styled Components
    Configurações de temas
    Tipagem de temas
    Estilos globais
    Cores e fonte

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-export function App() {
+import { ThemeProvider } from "styled-components";
 
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+import { Button } from "./components/Button";
+
+import { defaultTheme } from './assets/styles/themes/default';
+import { GlobalStyle } from "./assets/styles/global";
+
+export function App(){
+	// GlobalStyle deve estar dentro do ThemeProvider, caso contrário ele não terá acesso as variáveis do tema
+	// Posso colocar ele em qualquer ordem dentro do ThemeProvider
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<Button variant="primary"/>
+			<Button variant="secondary"/>
+			<Button variant="success"/>
+			<Button variant="danger"/>
+			<Button />
+
+			<GlobalStyle />
+		</ThemeProvider>
   )
 }
