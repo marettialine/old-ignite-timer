8521dfb1f4d5fff4be28c5b800680685bb205778
marettialine <alinesmaretti@gmail.com>
2023-07-11 22:22:52 -0300

    Default layout e Header

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,24 @@ import { Routes, Route } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
+  // O Route é de rotas encadeadas, então pra acessar o "/products" sempre terá que ter o "/admin" na frente
+  // https:localhost:3000/admin/products
+
+  /*
+    <Route path="/admin" element={<DefaultLayout />}>
+      <Route path="/products" element={<Home />} />
+    </Route>
+  */
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
