c176edc86de9b56b769f438af6c42f9963570a94
marettialine <alinesmaretti@gmail.com>
2023-07-14 12:04:52 -0300

    Validação formulário
    Validação usando React Hook Form e Zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
+import { ChangeEvent, useState } from 'react'
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod' // Não possui export default, por isso * as zod, caso contrário teria que colocar entre {} cada coisa que queríamos importar
 
 import {
   CountdownContainer,
@@ -10,16 +14,57 @@ import {
   TaskInput,
 } from './styles'
 
+// Schema: definir um formato e validar dados de um formulário baseado nesse formato
+// zod.object: foi usado object pois o retorno do nosso fomrulário é um objeto (data)
+const newCycleFormValidationSchema = zod.object({
+  // task tem que ser string com pelo menos uma tarefa, caso contrário mensagem de aviso
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos'),
+})
+
 export function Home() {
+  //  useForm: objeto que possui várias funções/variáveis para criar o formulário
+  /**
+   * register: adiciona input no formulário, tira a necessidade de colocar name no input
+   * function register(name, string){
+   *  return(
+   *    onChange: () => void,
+   *    onBlur: () => void,
+   *    onFocus: () => void,
+   *  )
+   * }
+   */
+
+  const { register, handleSubmit, watch, formState } = useForm({
+    // Adicionar dentro de zodResolver todo  lógica de validação
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  // Retorna os erros de validação do resolver
+  console.log(formState.errors)
+
+  // Observa o valor do input em tempo real, transforma form em controlled
+  // Controlled recria o componente inteiro ou somente o input nesse caso
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -37,6 +82,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos</span>
@@ -50,7 +96,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton type="submit">
+        <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
           Começar
         </StartCountdownButton>
